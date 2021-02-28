44d117faf07e250904cfafdfee7a6cd42fd21f24
Colk-tech <colk.tech@gmail.com>
2021-03-01 05:10:55 +0900

    [modify] Modified handlers

--- handlers/on_voice_state_changed.go
+++ handlers/on_voice_state_changed.go
@@ -1,8 +1,13 @@
 package handlers
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"github.com/bwmarrin/discordgo"
+)
 
 
 func OnVoiceStateChanged(session *discordgo.Session, event *discordgo.VoiceStateUpdate) {
-
+	user, _ := session.User(event.UserID)
+	if user.Bot {
+		return
+	}
 }
